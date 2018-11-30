1753460e21e9539bdbe150aee6545a0c37905a25
Maksym Zakharchuk <3maksym@gmail.com>
2018-11-30 16:29:34 +0100

    changes in webpack

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,5 +1,11 @@
-if(process.env.NODE_ENV === 'production'){
-    module.exports = require('./configureStore.prod')
-}else{
-    module.exports = require('./configureStore.dev')
+import {createStore, applyMiddleware} from 'redux'
+import rootReducer from '../_reducers'
+import thunk from 'redux-thunk';
+
+export default function configureStore(initialState){
+    return createStore(
+        rootReducer,
+        initialState,
+        applyMiddleware(thunk)
+    )
 }
